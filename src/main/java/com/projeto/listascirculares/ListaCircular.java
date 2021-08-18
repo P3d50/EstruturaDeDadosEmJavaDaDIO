f1d91b7c3ba447497e0622e3e90369f4dd9a81ab
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-08-18 08:48:29 -0300

    Lista circular
    remove()

--- src/main/java/com/projeto/listascirculares/ListaCircular.java
+++ src/main/java/com/projeto/listascirculares/ListaCircular.java
@@ -3,10 +3,41 @@ package com.projeto.listascirculares;
 public class ListaCircular<T> {
 
     private No<T> cabeca;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
 
-    
+    public void remove(int index){
+        if(index>=tamanhoLista)
+            throw new IndexOutOfBoundsException("O índice é maior qu o tamanho da lista");
+        No<T> noAuxiliar = this.cauda;
+            if(index==0){
+                this.cauda = this.cauda.getNoProximo();
+                this.cabeca.setNoProximo(this.cauda);
+            }else if(index==1){
+                this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+            }else {
+                for(int i =0;i<index-1;i++){
+                    noAuxiliar = noAuxiliar.getNoProximo();
+                }
+                noAuxiliar.setNoProximo(noAuxiliar.getNoProximo().getNoProximo());
+            }
+
+            this.tamanhoLista--;
+    }
+
+    private No<T> getNo(int index){
+        if(this.isEmpty()){
+            throw new IndexOutOfBoundsException("a lista esta vazia");
+        }
+        if(index==0){
+            return this.cauda;
+        }
+        No<T> noAux = this.cauda;
+        for(int i=0;(i<index)&&(noAux!=null);i++){
+            noAux=noAux.getNoProximo();
+        }
+        return noAux;
+    }
 
     public boolean isEmpty(){
         return this.tamanhoLista==0;
