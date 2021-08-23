8f818cca10e0380d36a2442baeab61d75d73ea83
Pedro Marcos Ramos <53919658+P3d50@users.noreply.github.com>
2021-08-23 07:44:36 -0300

    Implementação padrão das estruturas de daddos no Java
    Queue

---
+++ src/main/java/com/projeto/implementacaopadraojava/queue/Main.java
@@ -0,0 +1,29 @@
+package com.projeto.implementacaopadraojava.queue;
+
+import com.projeto.implementacaopadraojava.Carro;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String arg[]){
+        Queue<Carro> queue = new LinkedList<>();
+        queue.add(new Carro("Ford"));
+        queue.add(new Carro("Chev"));
+        queue.add(new Carro("Fiat"));
+        System.out.println(queue);
+        System.out.println(queue.add(new Carro("Peugeot")));
+        System.out.println(queue);
+        System.out.println(queue.offer(new Carro("Renault")));
+        System.out.println(queue);
+        System.out.println(queue.peek());
+        System.out.println(queue);
+        System.out.println(queue.poll());
+        System.out.println(queue);
+        System.out.println(queue.isEmpty());
+        System.out.println(queue.size());
+
+
+    }
+
+}
